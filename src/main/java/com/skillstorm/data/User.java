91e2352c583ff2655abc5abe4d93e7c4224702b8
roelcantu <roelhcantu@gmail.com>
2019-11-15 20:36:56 -0600

    Created User Class

---
+++ src/main/java/com/skillstorm/data/User.java
@@ -0,0 +1,65 @@
+package com.skillstorm.data;
+
+public class User {
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	
+	public User() {};
+	
+	public User(int userId, String firstName, String lastName, String userName, String password, String roleId) {
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", userName="
+				+ userName + ", password=" + password + "]";
+	}
+}
